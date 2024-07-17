97d4f68eea561dc86158fa40803f96bbdba0a367
alexandrpoliarush <oleksandr@renormalizing.com>
2024-07-17 08:41:01 +0300

    feat: add constants

---
+++ backend/src/modules/posts/constants.ts
@@ -0,0 +1,4 @@
+export const SOMETHING_SENT_WRONG = 'Something went wrong';
+export const INVALID_PAGE_QUERY = 'Invalid page query';
+export const INVALID_COUNT_QUERY = 'Invalid count query';
+export const ERROR_IN_PARSE_RSS = 'Failed to parse RSS';
--- backend/src/modules/posts/posts.controller.ts
+++ backend/src/modules/posts/posts.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { PostsService } from './posts.service';
 import { GetAllPostsQuery } from './types/get-all-posts.types';
 import { GetPostParams } from './types/get-post.types';
+import { ERROR_IN_PARSE_RSS, INVALID_COUNT_QUERY, INVALID_PAGE_QUERY, SOMETHING_SENT_WRONG } from './constants';
 
 export class PostsController {
 	constructor(private readonly postsService: PostsService) {}
@@ -10,11 +11,11 @@ export class PostsController {
 		try {
 			const { page, count, title } = req.query;
 			if (page && Number.isNaN(+page)) {
-				return res.status(400).json({ message: 'Invalid page query' });
+				return res.status(400).json({ message: INVALID_PAGE_QUERY });
 			}
 
 			if (count && Number.isNaN(+count)) {
-				return res.status(400).json({ message: 'Invalid count query' });
+				return res.status(400).json({ message: INVALID_COUNT_QUERY });
 			}
 
 			const [result, error] = await this.postsService.getAllPosts(+(page || 1), +(count || 10), title);
@@ -25,7 +26,7 @@ export class PostsController {
 			return res.status(200).json(result);
 		} catch (error) {
 			console.log('Failed to get all posts:', error);
-			return res.status(500).json({ message: 'Something went wrong' });
+			return res.status(500).json({ message: SOMETHING_SENT_WRONG });
 		}
 	}
 
@@ -40,7 +41,7 @@ export class PostsController {
 			return res.json(post)
 		} catch (error) {
 			console.log('Failed to get post:', error);
-			return res.status(500).json({ message: 'Something went wrong' });
+			return res.status(500).json({ message: SOMETHING_SENT_WRONG });
 		}
 	}
 
@@ -50,7 +51,7 @@ export class PostsController {
 			return res.json({ message: 'OK' });
 		} catch (error) {
 			console.log('Failed to parse RSS:', error);
-			return res.status(500).json({ message: 'Failed to parse RSS' });
+			return res.status(500).json({ message: ERROR_IN_PARSE_RSS });
 		}
 	}
 }
