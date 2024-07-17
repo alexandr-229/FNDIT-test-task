d773f06462542bfedca298d10b8501ccab459991
alexandrpoliarush <oleksandr@renormalizing.com>
2024-07-17 07:23:11 +0300

    feat: add express to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,16 @@
-const message: string = 'Message';
-console.log(message);
+import cors from 'cors';
+import express from 'express';
+import { config } from 'dotenv';
+
+config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(PORT, () => {
+	console.log(`App running on port ${PORT}`)
+})
